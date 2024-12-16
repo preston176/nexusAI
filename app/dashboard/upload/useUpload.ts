493eb34979817afb9fb25d577fe25e8962aa4619
preston176 <prestonnyamweya@gmail.com>
2024-12-16 12:55:26 +0300

    feat: implement file upload hook with progress tracking and Firestore integration

--- app/dashboard/upload/useUpload.ts
+++ app/dashboard/upload/useUpload.ts
@@ -1,6 +1,12 @@
 "use client"
 
-import { useState } from "react"
+import { db, storage } from "@/firebase";
+import { useUser } from "@clerk/nextjs";
+import { doc, setDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import {v4 as uuidv4} from "uuid";
 
 export enum StatusText {
     UPLOADING = "File is Uploading...",
@@ -15,6 +21,43 @@ function useUpload() {
     const [progress, setProgress] = useState<number | null>(null);
     const [fileId, setFileId] = useState<string | null>(null);
     const [status, setStatus] = useState<Status | null>(null);
+
+    const {user} = useUser();
+
+    const router = useRouter();
+
+    const handleUpload = async (file:File) => {
+        if(!file || !user) return;
+
+        //TODO: FREE / PRO USER LIMITATIONS
+        const fileIdToUploadTo = uuidv4(); 
+        const storageRef = ref(storage, `users/${user.id}/files/${fileIdToUploadTo}`)
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on("state_changed", (snapshot)=> {
+            const percent = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+            setProgress(percent);
+            setStatus(StatusText.UPLOADING);
+        }, (error) => {
+            console.error("Error uploading the file", error)
+        }, async () => {
+            setStatus(StatusText.UPLOADED);
+            const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+            setStatus(StatusText.SAVING);
+            await setDoc(doc(db, "users", user.id, "files", fileIdToUploadTo), {
+               name: file.name,
+               size: file.size,
+               type: file.type,
+               downloadURL: downloadURL,
+               ref: uploadTask.snapshot.ref.fullPath,
+               createdAt: new Date(), 
+            })
+            setStatus(StatusText.GENERATING);
+            // Generate AI Vector Embeddings
+            setFileId(fileIdToUploadTo);
+        })
+    }
+    return {progress,status, fileId, handleUpload}
 }
 
 export default useUpload
