ea1546165392ebd07d627110022ab316957ca2f3
preston176 <prestonnyamweya@gmail.com>
2025-01-08 13:44:08 +0300

    feat: add react-markdown dependency and update next.config.ts for additional image hostname

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "i.imgur.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
     ],
   },
