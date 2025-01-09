5b37b55f65703b6eefff873a998364c3f8107597
preston176 <prestonnyamweya@gmail.com>
2025-01-09 19:15:53 +0300

    feat: add Google APIs script to DashboardLayout component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,12 @@
 import Header from "@/components/Header"
 import { ClerkLoaded } from "@clerk/nextjs"
+import Head from "next/head"
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
+    <Head>
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+
+    </Head>
     return (
         <>
             <ClerkLoaded><div className="flex flex-col flex-1 h-screen">
