aa9a178572d8979afc6faff8a973697fdff2cc40
preston176 <prestonnyamweya@gmail.com>
2025-01-09 18:48:02 +0300

    feat: refactor firebaseAdmin to use ES module import for service key

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -3,12 +3,13 @@ import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-const serviceKey = require("./service_key.json");
+import serviceKey from "./service_key.json";
 
 let app: App;
 
 if (getApps().length === 0) {
   app = initializeApp({
+    // @ts-expect-error: firebase admin servicekey
     credential: cert(serviceKey),
   });
 } else {
