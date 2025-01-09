c4bc23f29d2f5945fcd5ce7fa0d064121f261f7f
preston176 <prestonnyamweya@gmail.com>
2025-01-09 13:11:18 +0300

    feat: add useSubscription hook for managing user file limits and membership status

---
+++ hooks/useSubscription.ts
@@ -0,0 +1,61 @@
+"use client";
+
+import { db } from "@/firebase";
+import { useUser } from "@clerk/nextjs";
+import { collection, doc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useCollection, useDocument } from "react-firebase-hooks/firestore";
+
+// No of docs
+const PRO_LIMIT = 1000;
+const FREE_LIMIT = 5;
+
+function useSubscription() {
+  const [hasActiveMembership, setHasActiveMembership] = useState(null);
+  const [isOverFileLimit, setIsOverFileLimit] = useState(false);
+
+  const { user } = useUser();
+
+  //   Listen to user doc
+  const [snapshot, loading, error] = useDocument(
+    user && doc(db, "users", user.id),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
+
+  //   Listen to the files collection
+  const [filesSnapshot, filesLoading] = useCollection(
+    user && collection(db, "users", user?.id, "files")
+  );
+
+  // Real time listener to our logged in state
+  useEffect(() => {
+    if (!snapshot) return;
+
+    // get all of the data from the users doc
+    const data = snapshot.data();
+    if (!data) return;
+
+    setHasActiveMembership(data.hasActiveMembership);
+  }, [snapshot]);
+
+  useEffect(() => {
+    if (!filesSnapshot || hasActiveMembership === null) return;
+
+    const files = filesSnapshot.docs;
+
+    const usersLimit = hasActiveMembership ? PRO_LIMIT : FREE_LIMIT;
+
+    console.log(
+      `--- Checking if user is over file limit ---`,
+      files.length,
+      usersLimit
+    );
+    setIsOverFileLimit(files.length >= usersLimit);
+  }, [filesSnapshot, hasActiveMembership]);
+
+  return { hasActiveMembership, loading, error, isOverFileLimit, filesLoading };
+}
+
+export default useSubscription;
