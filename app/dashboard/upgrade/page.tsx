33adc5e799f41296edbed32d3a6201632d2bf621
preston176 <prestonnyamweya@gmail.com>
2025-01-09 17:43:57 +0300

    feat: prevent transaction initiation for users with active membership and update Paystack public key usage

--- app/dashboard/upgrade/page.tsx
+++ app/dashboard/upgrade/page.tsx
@@ -33,6 +33,9 @@ function PricingPage() {
 
         startTransition(async () => {
             // Request initialization on backend to create a Paystack transaction
+            if (hasActiveMembership === true) {
+                return;
+            }
             const response = await fetch("/api/paystack", {
                 method: "POST",
                 body: JSON.stringify({ email: userDetails.email, amount: 40000, userId: user.id }), // Amount in kobo (1 Ksh = 100 Kobo)
@@ -46,7 +49,7 @@ function PricingPage() {
             if (data) {
                 const paystack = new PaystackPop();
                 paystack.newTransaction({
-                    key: "pk_test_389e4694979a1ec6e77ad171fe731628528be254" , // Your public key
+                    key: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY!, // Your public key
                     email: userDetails.email!,
                     amount: 40000, // Amount in kobo
                     onCancel: () => {
@@ -64,105 +67,105 @@ function PricingPage() {
                         router.push(`/dashboard?upgrade=true`);
                     },
                 });
-    } else {
-        console.error("Error initializing Paystack transaction", data);
-}
+            } else {
+                console.error("Error initializing Paystack transaction", data);
+            }
         });
     };
 
-return (
-    <>
-        <Head>
-            <script src="https://js.paystack.co/v1/inline.js" async></script>
-        </Head>
-        <div>
-            <div className="py-25 sm:py-32">
-                <div className="max-w-4xl mx-auto">
-                    <h2 className="text-base font-semibold leading-7 text-blue-500">Pricing</h2>
-                    <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                        Supercharge your Document Companion
-                    </p>
-                </div>
-
-                <p className="mx-auto mt-6 max-w-2xl px-10 text-center text-lg leading-8 text-gray-600">
-                    Select an affordable plan that&apos;s packed with the best features for interacting with your PDFs, enhancing productivity and streamlining your workflow
-                </p>
-                <div className="max-w-md mx-auto grid grid-cols-1 md:grid-cols-2 md:max-w-2xl gap-8 lg:max-w-4xl">
-                    <div className="ring-1 ring-gray-200 p-8 h-fit pb-12 rounded-3xl">
-                        <h3 className="text-lg font-semibold leading-8 text-gray-900">Starter Plan</h3>
-                        <p className="mt-4 text-sm leading-6 text-gray-600">
-                            Explore Core Features at No Cost
+    return (
+        <>
+            <Head>
+                <script src="https://js.paystack.co/v1/inline.js" async></script>
+            </Head>
+            <div>
+                <div className="py-25 sm:py-32">
+                    <div className="max-w-4xl mx-auto">
+                        <h2 className="text-base font-semibold leading-7 text-blue-500">Pricing</h2>
+                        <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                            Supercharge your Document Companion
                         </p>
-                        <p className="mt-6 flex items-baseline gap-x-1">
-                            <span className="text-4xl font-bold tracking-tight text-gray-900">Free</span>
-                        </p>
-                        <ul className="mt-8 space-y-3 text-sm leading-6 text-gray-600">
-                            <li className="flex gap-x-3">
-                                <CheckIcon className="h-6 w-5 flex-none text-blue-600" />
-                                Store up to 5 Documents
-                            </li>
-                            <li className="flex gap-x-3">
-                                <CheckIcon className="h-6 w-5 flex-none text-blue-600" />
-                                Up to 10 Messages per document <br />
-                                Resets every 48 Hours
-                            </li>
-                            <li className="flex gap-x-3">
-                                <CheckIcon className="h-6 w-5 flex-none text-blue-600" />
-                                Try out the AI Chat Functionality
-                            </li>
-                        </ul>
                     </div>
 
-                    <div className="ring-2 ring-blue-600 rounded-3xl p-8">
-                        <h3 className="text-lg font-semibold leading-8 text-blue-600">Pro Plan</h3>
-                        <p className="mt-4 text-sm leading-6 text-gray-600">
-                            Maximize your Productivity with PRO Features
-                        </p>
-                        <p className="mt-6 flex items-baseline gap-x-1">
-                            <span className="text-4xl font-bold tracking-tighter text-gray-900">Ksh 400</span>
-                            <span className="text-sm font-semibold leading-6 text-gray-600">/ Month</span>
-                        </p>
+                    <p className="mx-auto mt-6 max-w-2xl px-10 text-center text-lg leading-8 text-gray-600">
+                        Select an affordable plan that&apos;s packed with the best features for interacting with your PDFs, enhancing productivity and streamlining your workflow
+                    </p>
+                    <div className="max-w-md mx-auto grid grid-cols-1 md:grid-cols-2 md:max-w-2xl gap-8 lg:max-w-4xl">
+                        <div className="ring-1 ring-gray-200 p-8 h-fit pb-12 rounded-3xl">
+                            <h3 className="text-lg font-semibold leading-8 text-gray-900">Starter Plan</h3>
+                            <p className="mt-4 text-sm leading-6 text-gray-600">
+                                Explore Core Features at No Cost
+                            </p>
+                            <p className="mt-6 flex items-baseline gap-x-1">
+                                <span className="text-4xl font-bold tracking-tight text-gray-900">Free</span>
+                            </p>
+                            <ul className="mt-8 space-y-3 text-sm leading-6 text-gray-600">
+                                <li className="flex gap-x-3">
+                                    <CheckIcon className="h-6 w-5 flex-none text-blue-600" />
+                                    Store up to 5 Documents
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CheckIcon className="h-6 w-5 flex-none text-blue-600" />
+                                    Up to 10 Messages per document <br />
+                                    Resets every 48 Hours
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CheckIcon className="h-6 w-5 flex-none text-blue-600" />
+                                    Try out the AI Chat Functionality
+                                </li>
+                            </ul>
+                        </div>
 
-                        <Button
-                            disabled={loading || isPending}
-                            className="bg-blue-600 w-full text-white shadow-sm hover:bg-blue-500 mt-6 block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                            onClick={handleUpgrade}
-                        >
-                            {isPending || loading
-                                ? "Loading"
-                                : hasActiveMembership
-                                    ? "Manage My Subscription"
-                                    : "Upgrade To Pro"}
-                        </Button>
+                        <div className="ring-2 ring-blue-600 rounded-3xl p-8">
+                            <h3 className="text-lg font-semibold leading-8 text-blue-600">Pro Plan</h3>
+                            <p className="mt-4 text-sm leading-6 text-gray-600">
+                                Maximize your Productivity with PRO Features
+                            </p>
+                            <p className="mt-6 flex items-baseline gap-x-1">
+                                <span className="text-4xl font-bold tracking-tighter text-gray-900">Ksh 400</span>
+                                <span className="text-sm font-semibold leading-6 text-gray-600">/ Month</span>
+                            </p>
 
-                        <ul className="mt-8 space-y-3 text-sm leading-6 text-gray-600">
-                            <li className="flex gap-x-3">
-                                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
-                                Store <span className="font-bold">Unlimited</span> No. of Documents
-                            </li>
-                            <li className="flex gap-x-3">
-                                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
-                                Ability to Delete Documents
-                            </li>
-                            <li className="flex gap-x-3">
-                                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
-                                Unlimited Messaging
-                            </li>
-                            <li className="flex gap-x-3">
-                                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
-                                Full Power AI Chat Functionality with Memory Recall
-                            </li>
-                            <li className="flex gap-x-3">
-                                <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
-                                24-hour support
-                            </li>
-                        </ul>
+                            <Button
+                                disabled={loading || isPending}
+                                className="bg-blue-600 w-full text-white shadow-sm hover:bg-blue-500 mt-6 block rounded-md px-3 py-2 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                                onClick={handleUpgrade}
+                            >
+                                {isPending || loading
+                                    ? "Loading"
+                                    : hasActiveMembership
+                                        ? "Manage My Subscription"
+                                        : "Upgrade To Pro"}
+                            </Button>
+
+                            <ul className="mt-8 space-y-3 text-sm leading-6 text-gray-600">
+                                <li className="flex gap-x-3">
+                                    <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                                    Store  upto<span className="font-bold">100</span> Documents
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                                    Ability to Delete Documents
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                                    Unlimited Messaging
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                                    Full Power AI Chat Functionality with Memory Recall
+                                </li>
+                                <li className="flex gap-x-3">
+                                    <CheckIcon className="h-6 w-5 flex-none text-indigo-600" />
+                                    24-hour support
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </>
-);
+        </>
+    );
 }
 
 export default PricingPage;
