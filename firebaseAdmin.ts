25cd4b92ed507f7a1fdc8a5aa36a27104095837e
preston176 <prestonnyamweya@gmail.com>
2025-01-09 17:27:45 +0300

    feat: add Firebase storage integration and update package dependencies

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,21 +1,21 @@
-import {initializeApp, getApps, App, getApp, cert} from "firebase-admin/app"
+import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 
-import { getFirestore } from "firebase-admin/firestore"
+import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
 
-const serviceKey = require('./service_key.json')
+const serviceKey = require("./service_key.json");
 
 let app: App;
 
-if (getApps().length ===0) {
-    app= initializeApp({
-        credential: cert(serviceKey),
-    })
+if (getApps().length === 0) {
+  app = initializeApp({
+    credential: cert(serviceKey),
+  });
+} else {
+  app = getApp();
 }
- else {
-    app = getApp();
- }
 
- const adminDb = getFirestore(app);
+const adminDb = getFirestore(app);
+const adminStorage = getStorage(app);
 
-
- export {app as adminApp, adminDb}
+export { app as adminApp, adminDb, adminStorage };
